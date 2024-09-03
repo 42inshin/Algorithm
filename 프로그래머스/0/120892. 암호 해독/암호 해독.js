6f862d8eb3c4288d47ec4394c1fcee6b8e6498a2
INHO SHIN <inshin@student.42seoul.kr>
2024-09-03 21:39:52 +0900

    [level 0] Title: 암호 해독, Time: 0.22 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,4 +1,11 @@
 function solution(cipher, code) {
+    return cipher.split("").filter((_, index) => (index + 1) % code == 0).join("")
+}
+
+
+/*
+// 1번째 풀이
+function solution(cipher, code) {
     var arr = cipher.split("");
     var num = Math.floor(arr.length / code)
     var answer = ""
@@ -9,3 +16,4 @@ function solution(cipher, code) {
     
     return answer
 }
+*/
