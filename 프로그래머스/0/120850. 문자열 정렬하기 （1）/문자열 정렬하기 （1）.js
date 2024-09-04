6a5906a7ff66239f74b4b2cddab03d337d6fb2b2
INHO SHIN <inshin@student.42seoul.kr>
2024-09-04 23:18:22 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,4 +1,13 @@
 function solution(my_string) {
+    return my_string
+        .split("")
+        .filter(v => !isNaN(v))
+        .map(v => parseInt(v))
+        .sort((a,b) => a - b)
+}
+
+/*
+function solution(my_string) {
     var answer = []
     
     var s = my_string.split("").filter(e => {
@@ -9,3 +18,4 @@ function solution(my_string) {
     
     return answer.sort((a,b) => a - b)
 }
+*/
