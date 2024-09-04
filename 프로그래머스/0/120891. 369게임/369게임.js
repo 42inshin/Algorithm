3c67c257371c9029dce9ffe9051c5c437cc088d2
INHO SHIN <inshin@student.42seoul.kr>
2024-09-05 00:25:44 +0900

    [level 0] Title: 369게임, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,3 +1,7 @@
 function solution(order) {
-    return order.toString().split("").filter(v => v != "0" && v*1 % 3 == 0).length
+    return order.toString().split(/[369]/).length - 1
 }
+
+// function solution(order) {
+//     return order.toString().split("").filter(v => v != "0" && v*1 % 3 == 0).length
+// }
