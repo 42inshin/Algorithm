047958c56a12ff06751b2228aa477ebb494da548
INHO SHIN <inshin@student.42seoul.kr>
2024-09-06 01:43:49 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,7 +1,14 @@
+/*
 function solution(array, n) {
     var copy = [...array].sort((a,b) => a - b)
     var a = copy.map(v => Math.abs(v - n))
     var min = Math.min(...a)
     
     return copy[a.indexOf(min)]
+}
+*/
+
+// 한 줄로도 표현 가능
+function solution(array, n) {
+    return array.sort((a,b) => Math.abs(n - a) - Math.abs(n - b) || a - b)[0]
 }
