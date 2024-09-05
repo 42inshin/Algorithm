9dbc5adf4e8404bd91359899771b521cb5ec498c
INHO SHIN <inshin@student.42seoul.kr>
2024-09-05 17:48:17 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,14 +1,18 @@
+
+// 한 줄 코드
+function solution(my_string) {
+    return my_string.split(/[a-zA-Z]/).reduce((acc, v) => acc + Number(v), 0)
+}
+// Number에 빈문자열, 공백을 넣으면 0이 나오며, 숫자가 아닌걸 넣으면 NaN이 나온다. 위 코드에서는 "" 이 포함되어 있으며 0으로 변경되어 동작한다.
+
+/*
 function solution(my_string) {
     var sum = 0;
-    
     my_string.split(/[a-zA-Z]/).map(v => {
         if (parseInt(v)) {
             sum += parseInt(v)
         }
     })
-    
     return sum
 }
-
-// 한 줄 코드
-//  console.log(my_string.split(/[a-zA-Z]/).reduce((acc, v) => acc + Number(v), 0))
+*/
