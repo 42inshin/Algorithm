b5f77aa9322ccacfb92f049e749e1ed424cefe27
INHO SHIN <inshin@student.42seoul.kr>
2024-09-05 17:08:46 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,6 +1,6 @@
 function solution(balls, share) {
     var answer = acc(balls) / (acc(balls - share) * acc(share));
-    return Math.round(answer);
+    return Math.round(answer); // 반올림을 안쓰면 정확한 값이 안나오는 경우가 생긴다.
 }
 
 function acc(n) {
@@ -11,5 +11,6 @@ function acc(n) {
     return result;
 }
 
-//입력값 〉 30, 15
+// round 안쓸 시 fail 나오는 테스트 케이스
+// 입력값 〉 30, 15
 // 기댓값 〉 155117520
