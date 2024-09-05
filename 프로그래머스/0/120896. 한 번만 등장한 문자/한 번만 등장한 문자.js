3a6a7d5b1a780ffb2a0a0049653dd1ac59332b94
INHO SHIN <inshin@student.42seoul.kr>
2024-09-06 02:02:39 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,3 +1,4 @@
+/*
 function solution(s) {
     var set = new Set(s);
     var answer = "";
@@ -8,4 +9,10 @@ function solution(s) {
     })
     
     return answer;
+}
+*/
+
+// 다른 사람 풀이
+function solution(s) {
+    return [...s].filter(c => s.indexOf(c) == s.lastIndexOf(c)).sort().join('')
 }
