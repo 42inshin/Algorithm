953cd0196dcf18ec62c1721567a08332f1191a6e
INHO SHIN <inshin@student.42seoul.kr>
2024-09-06 21:52:13 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,3 +1,4 @@
+/*
 function solution(my_str, n) {
     var answer = [];
     var s = [...my_str]
@@ -5,4 +6,12 @@ function solution(my_str, n) {
         answer.push(s.splice(0, n).join(''));
     }
     return answer;
+}
+*/
+
+// 더 쉬운 방법
+// . : 모든 문자
+// {1, n} 최소 1번에서 최대 n번까지 임의의 문자와 매치. 즉 문자열을 n개의 문자 단위로 잘라 배열로 반환합니다.
+function solution(my_str, n) {
+    return my_str.match(new RegExp(`.{1,${n}}`, 'g'))
 }
