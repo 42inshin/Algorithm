a12cd38cbd48caf8afdf5d47c3dd69310a338f03
INHO SHIN <inshin@student.42seoul.kr>
2024-09-06 21:30:23 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,3 +1,4 @@
+/*
 function solution(bin1, bin2) {
     return (convert2to10(bin1) + convert2to10(bin2)).toString(2)
 }
@@ -10,4 +11,12 @@ function convert2to10(s) {
            acc += 2 ** i
    }
     return acc
+}
+*/
+
+// 쉬운 풀이.
+// parseInt() 함수는 문자열 인자를 파싱하여 특정 진수(수의 진법 체계에서 기준이 되는 값)의 정수를 구할 수 있다.
+
+function solution(bin1, bin2) {
+    return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2)
 }
