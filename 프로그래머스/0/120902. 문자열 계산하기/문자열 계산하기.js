e96a6bd44eb49a0cd581d1eb3d034a5dab0d4374
INHO SHIN <inshin@student.42seoul.kr>
2024-09-07 19:11:28 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,15 +1,15 @@
 function solution(my_string) {
     var arr = my_string.split(' ');
     var answer = 0;
-    var a = 1
+    var sign = 1
     
     arr.forEach(v => {
         if (v == '+') {
-            a = 1;
+            sign = 1;
         } else if (v == '-') {
-            a = -1;
+            sign = -1;
         } else {
-            answer += parseInt(v) * a
+            answer += parseInt(v) * sign
         }
     })
     return answer;
