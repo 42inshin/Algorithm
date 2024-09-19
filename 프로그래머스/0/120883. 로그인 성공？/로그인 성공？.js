32ea85ca9fe3e157b52a6b58e4e65931571bd397
INHO SHIN <inshin@student.42seoul.kr>
2024-09-19 19:53:54 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,6 +1,16 @@
+/*
 function solution(id_pw, db) {
     var index = db.findIndex(v => v[0] == id_pw[0]);
     if (index < 0) 
         return "fail";
     return db[index][1] == id_pw[1] ? "login" : "wrong pw";
+}
+*/
+
+// Map 써보기
+function solution(id_pw, db) {
+    const [id, pw] = id_pw;
+    const map = new Map(db)
+    
+    return map.get(id) ? map.get(id) == pw ? "login" : "wrong pw" : "fail"
 }
