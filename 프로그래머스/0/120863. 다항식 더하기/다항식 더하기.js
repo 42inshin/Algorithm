3fddba4907757b00b0a580f1e7cef3a4feab2aa7
INHO SHIN <inshin@student.42seoul.kr>
2024-09-22 00:22:50 +0900

    [level 0] Title: 다항식 더하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -0,0 +1,24 @@
+function solution(polynomial) {
+    var answer = [0,0]
+    var arr = [...polynomial].filter(v => v != " ").join("").split("+");
+    arr.forEach(v => {
+        var hasX = v.indexOf("x");
+        var num = v.replace('x', '')
+        
+        if (hasX == 0)
+            answer[0] += 1;
+        else if (hasX > 0) {
+            answer[0] += +num
+        } else {
+            answer[1] += +num
+        }
+    })
+    
+    if (answer[0] == 0)
+        return answer[1].toString();
+    else if (answer[1] == 0)
+        return answer[0] == 1 ? "x" : answer[0] + "x";
+    else
+        return (answer[0] == 1 ? "x" : answer[0] + "x") + " + " + answer[1];
+    
+}
