222873ba368b540e845fdb665cf6a668976f50ed
INHO SHIN <inshin@student.42seoul.kr>
2024-09-22 00:04:07 +0900

    [level 0] Title: 특이한 정렬, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,7 +1,8 @@
 function solution(numlist, n) {
     return numlist.sort((a,b) => {
-        if (Math.abs(n-a) == Math.abs(n-b))
-            return b -a
-        return Math.abs(n-a) - Math.abs(n-b)
+        var answer = Math.abs(n-a) - Math.abs(n-b)
+        if (answer == 0)
+            return b - a;
+        return answer;
     })
 }
