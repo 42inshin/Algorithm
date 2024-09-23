94d9da2596b699f7d5cd19eb06cd4c7f6bfeff46
INHO SHIN <inshin@student.42seoul.kr>
2024-09-24 02:40:31 +0900

    [level 2] Title: 점 찍기, Time: 28.88 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -1,8 +1,10 @@
+
 function solution(k, d) {
     var answer = 0;
     const dd = d * d;
     var x = 0, y = 0;
     
+    // y 값을 최대 위치에 둠.
     while (y < d) {
         y += k;
     }
@@ -10,8 +12,8 @@ function solution(k, d) {
     while (x <= d) {
         while (y >= 0) {
             if (y * y + x * x <= dd) {
-                var validPots = Math.floor(y / k) + 1;
-                answer += validPots;
+                var validDots = Math.floor(y / k) + 1;
+                answer += validDots;
                 break ;
             }
             y -= k;
