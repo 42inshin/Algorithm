582f7d11f0b938dbc8b258297892c228c8a8289b
INHO SHIN <inshin@student.42seoul.kr>
2024-09-25 00:50:43 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.js
@@ -1,4 +1,4 @@
-function solution(arr, n) {
+function solution1(arr, n) {
     var len = arr.length;
     if (len % 2 != 0) {
         return arr.map((v, i) => {
@@ -9,4 +9,8 @@ function solution(arr, n) {
             return (i % 2 != 0) ? v + n : v;
         });
     }
+}
+
+function solution(arr, n) {
+    return arr.map((v, i) => (arr.length - i) % 2 * n + v)
 }
