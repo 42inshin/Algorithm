30309de4877a5125414ba7c9b97db5b267e6fc56
INHO SHIN <inshin@student.42seoul.kr>
2024-09-26 21:24:40 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 43.62 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,17 +1,16 @@
 function solution(sequence, k) {
     let start = 0, end = 0, sum = sequence[end];
     let len = sequence.length;
-    var answer = null;
+    let answer = [0, len - 1];
     
     while (start < len && end < len) {
-        if (sum < k) {
+        if (sum < k)
             sum += sequence[++end];
-        } else if (sum > k) {
+        else if (sum > k)
             sum -= sequence[start++];
-        } else {
-            if (!answer || answer[1] - answer[0] > end - start) {
+        else {
+            if (answer[1] - answer[0] > end - start)
                 answer = [start, end];
-            }
             sum -= sequence[start++];
         }
     }
