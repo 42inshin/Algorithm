1558d92595c055911478de0a1ee9e5cffe6dfd51
INHO SHIN <inshin@student.42seoul.kr>
2024-09-27 02:50:11 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+    var answer = a + b + c;
+    
+    if (a == b && b == c)
+        answer *= a ** 3 + b ** 3 + c ** 3;
+    if (a == b || b == c || c == a)
+        answer *= a ** 2 + b ** 2 + c ** 2;
+    
+    return answer;
+}
