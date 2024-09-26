a34fef57a9e15f067a6a6f0fbcaac324d34de51c
INHO SHIN <inshin@student.42seoul.kr>
2024-09-26 21:38:39 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 1.54 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -1,4 +1,9 @@
 function solution(arr) {
+    return arr.map(v => Array(v).fill(v)).flat()
+}
+
+
+function solution1(arr) {
     var answer = [];
     
     arr.forEach(v => answer.push(...Array.from({length: v}, () => v)));
