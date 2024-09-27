2886248a300eab591bff78a235d455ca30f1c8bc
INHO SHIN <inshin@student.42seoul.kr>
2024-09-27 09:56:43 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,3 +1,12 @@
 function solution(my_string) {
+    return my_string.trim().split(/ +/g)
+    // 정규식 설명
+    // ' ' (공백 문자): 단일 공백 문자를 의미합니다.
+    // + (플러스 기호): 앞의 공백 문자가 하나 이상 반복된다는 것을 나타냅니다.
+    // /: 정규식 패턴의 시작과 끝을 표시하는 구분자입니다.
+    // g (global 플래그): 문자열 전체에서 모든 패턴의 발생을 찾겠다는 의미입니다.
+}
+    
+function solution1(my_string) {
     return my_string.trim().split(" ").filter(v => v != "")
 }
