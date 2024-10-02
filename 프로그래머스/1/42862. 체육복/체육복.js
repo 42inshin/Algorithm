a2c2c6246b8458014c2cc3cc04396bb72b656411
INHO SHIN <inshin@student.42seoul.kr>
2024-10-02 16:03:51 +0900

    [level 1] Title: 체육복, Time: 0.39 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,26 +1,4 @@
 function solution(n, lost, reserve) {
-    lost.sort((a,b) => a - b);
-    // reserve.sort((a,b) => a - b);
-    
-    for (let man of lost) {
-        if (reserve.findIndex(v => v == man) > -1) {
-            reserve = reserve.filter(v => v != man);
-            lost = lost.filter(v => v != man)
-        }
-    }
-    
-    for (let man of lost) {
-        if (reserve.findIndex(v => v == man - 1) > -1) {
-            reserve = reserve.filter(v => v != man - 1);
-            lost = lost.filter(v => v != man)
-        } else if (reserve.findIndex(v => v == man + 1) > -1) {
-            reserve = reserve.filter(v => v != man + 1);
-            lost = lost.filter(v => v != man)
-        }
-    }
-    
-    return n - lost.length;
+    var answer = 0;
+    return answer;
 }
-//입력값 〉 5, [4, 5], [3, 4]
-//기댓값 〉 4
-
