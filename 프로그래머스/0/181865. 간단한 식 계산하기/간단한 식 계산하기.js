b758d474f24b4bdd3d2588ce9e9458ca61515033
INHO SHIN <inshin@student.42seoul.kr>
2024-10-04 12:20:35 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,9 @@
+function solution(binomial) {
+    const [a, op, b] = binomial.split(" ");
+    if (op == "+")
+        return +a + +b;
+    else if (op == "-")
+        return a - b;
+    else
+        return a * b;
+}
