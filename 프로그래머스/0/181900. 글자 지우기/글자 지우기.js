478c0c338089b87f30f376b8b7bd3fe76aa1c7e4
INHO SHIN <inshin@student.42seoul.kr>
2024-10-07 00:07:57 +0900

    [level 0] Title: 글자 지우기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,7 +1,12 @@
-function solution(my_string, indices) {
+// 내 풀이
+function solution1(my_string, indices) {
     indices.sort((a,b) => b - a);
     let copy = [...my_string]
     for (let v of indices)
         copy.splice(v, 1);
     return copy.join("");
+}
+
+function solution(my_string, indices) {
+    return [...my_string].filter((_, i) => !indices.includes(i)).join("")
 }
