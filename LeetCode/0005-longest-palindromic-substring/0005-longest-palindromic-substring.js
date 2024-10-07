0c6368cef4af77ee65fd7b9a27078fbeaf9f8ccc
INHO SHIN <inshin@student.42seoul.kr>
2024-10-07 20:33:08 +0900

    Time: 81 ms (60.64%), Space: 54.9 MB (31.25%) - LeetHub

--- LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
+++ LeetCode/0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -2,32 +2,36 @@
  * @param {string} s
  * @return {string}
  */
-var longestPalindrome = function(str) {
-    var reverse = [...str].reverse().join("")
-    var [s, e] = [-1, -1];
-    var answer = [];
+var longestPalindrome = function(s) {
+    // for 문을 통해서 처음부터 끝까지 갈건데, 양쪽으로 같은게 있으면 확장시켜 보자.
+    var maxStart = 0, maxLen = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] == reverse[i]) {
-            if (s == -1)
-                s = i;
-            e = i;
-        } else {
-            if (s != -1)
-                answer.push([e - s + 1, [s, e]]);
-            s = -1;
-            e = -1;
+    if (s.length == 1) return s;
+
+    // 양쪽 같은지 확인 하는 함수.
+    var checkPal = (start, end) => {
+        while (start >= 0 && start < s.length && end >= 0 && end < s.length) {
+            if (s[start] == s[end]) {
+                start--;
+                end++;
+            } else {
+               break;
+            }
+        }
+        return [start + 1, end - start]
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        var [start, len] = checkPal(i, i)
+        if (len > maxLen) {
+            maxLen = len;
+            maxStart = start;
+        }
+        var [start2, len2] = checkPal(i, i+1)
+        if (len2 > maxLen) {
+            maxLen = len2;
+            maxStart = start2;
         }
     }
-
-    if (s != -1)
-        answer.push([e - s + 1, [s, e]]);
-
-    answer.sort((a,b) => b[0] - a[0]);
-    if (answer.length > 0) {
-        var [count, [a, b]] = answer[0]
-        return str.slice(a, b+1);
-    } else {
-        return str[0]
-    }
+    return s.substring(maxStart, maxStart + maxLen - 1)
 };
