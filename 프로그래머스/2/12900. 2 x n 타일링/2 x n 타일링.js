9298e31fee1c75d54d06b0f41644b92a1888b22f
INHO SHIN <inshin@student.42seoul.kr>
2024-10-09 22:55:14 +0900

    [level 2] Title: 2 x n 타일링, Time: 3.13 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,21 @@
+function solution1(n) {
+    let dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    
+    return dp[n];
+}
+
+function solution(n) {
+    let dp = [1, 1, 2]
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] % 1000000007 + dp[i - 2] % 1000000007;
+    }
+    
+    return dp[n] % 1000000007;
+}
