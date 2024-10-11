87963bd1596851fa3f03ffab430fe4f2d209ed43
INHO SHIN <inshin@student.42seoul.kr>
2024-10-11 19:01:03 +0900

    [level 0] Title: 날짜 비교하기, Time: 5.04 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,4 +1,4 @@
-function solution(date1, date2) {
+function solution1(date1, date2) {
     if (date1[0] < date2[0])
         return 1;
     else if (date1[0] == date2[0]) {
@@ -10,4 +10,8 @@ function solution(date1, date2) {
         }
     }
     return 0;
+}
+
+function solution(date1, date2) {
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
