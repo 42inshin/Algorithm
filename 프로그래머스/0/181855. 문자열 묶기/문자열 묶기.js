3e5477e6f303385e246b8b84db286e0ca64f007c
INHO SHIN <inshin@student.42seoul.kr>
2024-10-13 04:32:29 +0900

    [level 0] Title: 문자열 묶기, Time: 5.56 ms, Memory: 50.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,8 @@
+function solution(strArr) {
+    var map = new Map();
+    
+    strArr.forEach(v => {
+        map.set(v.length, map.get(v.length) + 1 || 1);
+    });
+    return Math.max(...[...map].map(v => v[1]));
+}
