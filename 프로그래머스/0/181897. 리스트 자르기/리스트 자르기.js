64535b3d144822be0bbf4c15f72617463ef1eac4
INHO SHIN <inshin@student.42seoul.kr>
2024-10-15 19:31:58 +0900

    [level 0] Title: 리스트 자르기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,4 +1,4 @@
-function solution(n, slicer, num_list) {
+function solution1(n, slicer, num_list) {
     let [a,b,c] = slicer;
     
     switch (n) {
@@ -13,4 +13,15 @@ function solution(n, slicer, num_list) {
         default:
             break;
     }
+}
+
+function solution(n, slicer, num_list) {
+    const obj = {
+        1: ([a,b,c], num_list) => num_list.slice(0, b + 1),
+        2: ([a,b,c], num_list) => num_list.slice(a),
+        3: ([a,b,c], num_list) => num_list.slice(a, b + 1),
+        4: ([a,b,c], num_list) => num_list.slice(a, b + 1).filter((_, i) => i % c == 0)
+    }
+    
+    return obj[n](slicer, num_list);
 }
