f250078893e054a0771bc9aa7a17259e21297606
INHO SHIN <inshin@student.42seoul.kr>
2024-10-19 08:20:18 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2.17 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,4 +1,4 @@
-function solution(arr, k) {
+function solution1(arr, k) {
     var answer = new Set(arr);
     
     answer = [...answer].slice(0, k);
@@ -7,5 +7,10 @@ function solution(arr, k) {
         answer.push(-1)
     }
     
-    return  answer;
+    return answer;
+}
+
+// 한줄 코드
+function solution(arr, k) {
+    return [...new Set(arr), ...new Array(k).fill(-1)].slice(0, k)
 }
