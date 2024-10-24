f7d8de1fe24a4864b5c00edd55aa158ed7d1988c
INHO SHIN <inshin@student.42seoul.kr>
2024-10-24 18:26:20 +0900

    [level 1] Title: 행렬의 덧셈, Time: 11.40 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,8 +1,3 @@
 function solution(arr1, arr2) {
-    arr1.forEach((v, i) => {
-        v.forEach((w, j) => {
-            arr1[i][j] += arr2[i][j]
-        })
-    })
-    return arr1;
+    return arr1.map((v, i) => v.map((w, j) => w + arr2[i][j]))
 }
