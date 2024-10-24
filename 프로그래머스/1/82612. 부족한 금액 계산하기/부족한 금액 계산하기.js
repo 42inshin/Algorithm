918974206bb1e4e7c5fd82de849fd087cbb1e8c0
INHO SHIN <inshin@student.42seoul.kr>
2024-10-24 18:18:58 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,4 +1,4 @@
-function solution(price, money, count) {
+function solution1(price, money, count) {
     var answer = 0;
     
     for (let i = 1; i <= count; i++) {
@@ -6,4 +6,11 @@ function solution(price, money, count) {
     }
     
     return answer > money ? answer - money : 0;
+}
+
+// 가우스 공식 이용
+
+function solution(price, money, count) {
+    var answer = price * count * (count + 1) / 2 - money;
+    return answer > 0 ? answer : 0;
 }
