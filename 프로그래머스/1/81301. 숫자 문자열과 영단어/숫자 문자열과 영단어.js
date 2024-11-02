4738646b61bfb0455238dd4e3cf6bc1055e80e9b
INHO SHIN <inshin@student.42seoul.kr>
2024-11-02 18:24:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,9 +1,8 @@
 function solution(s) {
     let numArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    let numStr = "0123456789"
     
     numArr.forEach((v, i) => {
-        s = s.replaceAll(v, numStr[i])
+        s = s.replaceAll(v, i)
     });
     return parseInt(s);
 }
