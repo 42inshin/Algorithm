c0156549bd35792add8b01127655cb5151f163e2
INHO SHIN <inshin@student.42seoul.kr>
2025-11-15 23:51:28 +0900

    [level 1] Title: 카드 뭉치, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,16 +1,10 @@
 function solution(cards1, cards2, goal) {
-    let answer = "Yes"
     let i1 = 0;
     let i2 = 0;
     for (let i = 0; i < goal.length; i++) {
-        if (goal[i] == cards1[i1]) {
-            i1++;
-        } else if (goal[i] == cards2[i2]) {
-            i2++;
-        } else {
-            answer = "No"
-            break;
-        }
+        if (goal[i] == cards1[i1]) i1++;
+        else if (goal[i] == cards2[i2]) i2++;
+        else return "No";
     }
-    return answer;
+    return "Yes"
 }
