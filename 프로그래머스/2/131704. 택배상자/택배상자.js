95f8dadd49ea2ed57a4ab1220a145df49f9b331c
INHO SHIN <inshin@student.42seoul.kr>
2025-11-15 19:02:02 +0900

    [level 2] Title: 택배상자, Time: 142.50 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -3,35 +3,23 @@
 // 2. 맞음. 꺼내기
 
 function solution(order) {
-    let answer = 0;
+    let total = 0;
+    let box = 1;
     const stack = [];
-    const len = order.length
-    const arr = Array(len).fill(1).map((e,i) => e + i)
-
-    let j = 0; // order index
     
-    for (let i = 0; i < arr.length; i++) {
-        // 순서대로 안맞으면 넣는다.
-        if (order[j] !== arr[i] && order[j] !== stack.at(-1)) {
-            stack.push(arr[i]);
-        } else if (order[j] === arr[i]) {
-            answer++;
-            j++;
-        } else if (order[j] === stack.at(-1)) {
-            while (order[j] === stack.at(-1)) {
-                stack.pop();
-                answer++;
-                j++;
-            }
-            stack.push(arr[i]);
-        }   
+    while (true) {
+        if (order.length === total) break;
+        if (order[total] === box) {
+            total++;
+            box++;
+        } else if (order[total] === stack.at(-1)) {
+            stack.pop();
+            total++;
+        } else if (order[total] !== stack.at(-1)) {
+            if (order[total] < stack.at(-1)) break;
+            stack.push(box)
+            box++;
+        }
     }
-    
-    while (stack.length && order[j] === stack.at(-1)) {
-        stack.pop();
-        answer++;
-        j++;
-    }
-    
-    return answer;
+    return total;
 }
