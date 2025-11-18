a44bb0896ad737add59185a07a6975cd84e87ea1
INHO SHIN <inshin@student.42seoul.kr>
2025-11-19 03:20:15 +0900

    [level 2] Title: 숫자 변환하기, Time: 5918.29 ms, Memory: 426 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538#) 
 
 ### 성능 요약
 
-메모리: 39.3 MB, 시간: 31.88 ms
+메모리: 426 MB, 시간: 5918.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 19일 03:17:45
+2025년 11월 19일 03:20:13
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -6,12 +6,9 @@ function solution(x, y, n) {
     for (let i = x; i < y + 1; i++) {
         if (dp[i] == 0 && i != x) continue;
         
-        if (i + n < y + 1)
-            dp[i + n] = Math.min(dp[i + n] !== 0 ? dp[i + n] : dp[i] + 1, dp[i] + 1);
-        if (i * 2 < y + 1)
-            dp[i * 2] = Math.min(dp[i * 2] !== 0 ? dp[i * 2] : dp[i] + 1, dp[i] + 1);
-        if (i * 3 < y + 1)
-            dp[i * 3] = Math.min(dp[i * 3] !== 0 ? dp[i * 3] : dp[i] + 1, dp[i] + 1);
+        dp[i + n] = Math.min(dp[i + n] !== 0 ? dp[i + n] : dp[i] + 1, dp[i] + 1);
+        dp[i * 2] = Math.min(dp[i * 2] !== 0 ? dp[i * 2] : dp[i] + 1, dp[i] + 1);
+        dp[i * 3] = Math.min(dp[i * 3] !== 0 ? dp[i * 3] : dp[i] + 1, dp[i] + 1);
     }
     
     return dp[y] == 0 ? -1 : dp[y] 
