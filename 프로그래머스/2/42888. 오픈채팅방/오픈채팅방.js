3f877be39e1669757cad8344c407672ac5f44f22
INHO SHIN <inshin@student.42seoul.kr>
2025-11-24 01:29:40 +0900

    [level 2] Title: 오픈채팅방, Time: 281.66 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -2,20 +2,15 @@ function solution(record) {
     const userMap = new Map();
     for (let s of record) {
         const [action, id, nickname] = s.split(" ");
-        if (action === "Enter")
+        if (action === "Enter" || action === "Change")
             userMap.set(id, nickname)
-        else if (action === "Change") {
-            userMap.set(id, nickname)
-        }
     }
     
     return record.map((s) => {
         const [action, id, nickname] = s.split(" ");
         let content = ""
-        if (action === "Enter")
-            content = "님이 들어왔습니다.";
-        else if (action === "Leave")
-            content = "님이 나갔습니다."
+        if (action === "Enter") content = "님이 들어왔습니다.";
+        else if (action === "Leave") content = "님이 나갔습니다."
         else return undefined; 
         return `${userMap.get(id)}${content}`
     }).filter(v => v != undefined);
