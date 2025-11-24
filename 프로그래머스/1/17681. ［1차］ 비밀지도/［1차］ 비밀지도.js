96fcbc8728930cc44bfed5ff7a44d5206d15cbfb
INHO SHIN <inshin@student.42seoul.kr>
2025-11-24 13:46:24 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,4 +1,10 @@
 function solution(n, arr1, arr2) {
+    return Array(n).fill().map((_, i) => {
+        return (arr1[i] | arr2[i]).toString(2).padStart(n, ' ').replace(/1/g, '#').replace(/0/g, ' ');
+    })
+}
+/*
+function solution(n, arr1, arr2) {
     var answer = [];
     
     for (let i = 0; i < n; i++) {
@@ -16,3 +22,4 @@ function solution(n, arr1, arr2) {
     
     return answer;
 }
+*/
