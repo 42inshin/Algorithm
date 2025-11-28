2f30e109e4994ab9ff53201bbe3179b75f2eefae
INHO SHIN <inshin@student.42seoul.kr>
2025-11-29 05:23:52 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 39.97 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,4 +1,27 @@
 function solution(queue1, queue2) {
-    var answer = -2;
-    return answer;
+    const arr = [...queue1, ...queue2]
+    const total = arr.reduce((total, v) => total + v, 0)
+    if (total % 2 === 1) return -1;
+    
+    const limit = arr.length * 2
+    let count = 0;
+    let l = 0;
+    let r = queue1.length - 1;
+    let sum = queue1.reduce((total, v) => total + v, 0);
+    let target = total / 2;
+    let len = arr.length;
+    
+    while (count < limit) {
+        if (sum === target) return count;
+        
+        if (sum > target) {
+            sum -= arr[l % len];
+            l++;
+        } else if (sum < target) {
+            r++;
+            sum += arr[r % len];
+        } 
+        count++;
+    }
+    return -1
 }
