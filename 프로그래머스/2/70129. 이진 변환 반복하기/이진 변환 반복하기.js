1dec3928ba90563dbf18b05a7b54ab649e1db25f
INHO SHIN <inshin@student.42seoul.kr>
2025-11-29 14:00:36 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 26.27 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const answer = [0, 0];
+    
+    while (s !== "1") {
+        // 0 제거
+        let temp = s.split("").filter(v => v != "0").join("");
+        answer[1] += s.length - temp.length
+        // 이진 변환
+        s = Number(temp.length).toString(2);
+        answer[0] += 1;
+    }
+    return answer;
+}
