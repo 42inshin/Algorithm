e30bf437f4a1c990278bd23655eb7a1cf7874fd7
INHO SHIN <inshin@student.42seoul.kr>
2025-12-01 13:27:56 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 13.67 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,14 +1,12 @@
 function solution(arr) {
     const answer = [0, 0]
     
-    const quadtree = (answer, arr) => {
-        // 전체 값이 같은지 확인
-        const len = arr.length;
-        const first = arr[0][0];
+    const quadtree = (x, y, size) => {
+        const first = arr[y][x];
         let same = true;
         
-        outer: for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr[i].length; j++) {
+        outer: for (let i = y; i < y + size; i++) {
+            for (let j = x; j < x + size; j++) {
                 if (arr[i][j] !== first) {
                     same = false;
                     break outer;
@@ -22,32 +20,13 @@ function solution(arr) {
         }
         
         // 4개로 쪼개기
-        let half = len / 2;
-        // top, left
-        const d1 = generateArray(arr, 0, half, 0, half);
-        // top, right
-        const d2 = generateArray(arr, 0, half, half, len);
-        // down, left
-        const d3 = generateArray(arr, half, len, 0, half);
-        // down, right
-        const d4 = generateArray(arr, half, len, half, len);
-        quadtree(answer, d1)
-        quadtree(answer, d2)
-        quadtree(answer, d3)
-        quadtree(answer, d4)
+        let half = size / 2;
+        quadtree(x, y, half)
+        quadtree(x + half, y, half)
+        quadtree(x, y + half, half)
+        quadtree(x + half, y + half, half)
     }
     
-    quadtree(answer, arr)
-    
+    quadtree(0, 0, arr.length)
     return answer;
-}
-
-function generateArray(arr, t, d, l, r) {
-    const newArr = [];
-    
-    for (let i = t; i < d; i++) {
-        newArr.push(arr[i].slice(l, r))
-    }
-    
-    return newArr
 }
