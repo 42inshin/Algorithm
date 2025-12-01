3a1daa03fc6d7921744c46e9d492544bdfa7091b
INHO SHIN <inshin@student.42seoul.kr>
2025-12-01 12:53:11 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 47.96 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,34 +1,24 @@
 function solution(arr) {
     const answer = [0, 0]
-    let len = arr.length;
     
     const quadtree = (answer, arr) => {
         // 전체 값이 같은지 확인
         const len = arr.length;
-        // const all = arr.reduce((a,b) => [...a, ...b], []);
-        const all = [];
-        for (let i = 0; i < arr.length; i++) {
-            // concat도 새 배열 생성이라 비추, push로만 붙이기
+        const first = arr[0][0];
+        let same = true;
+        
+        outer: for (let i = 0; i < arr.length; i++) {
             for (let j = 0; j < arr[i].length; j++) {
-                all.push(arr[i][j]);
+                if (arr[i][j] !== first) {
+                    same = false;
+                    break outer;
+                }
             }
         }
         // 배열 모든 값이 동일한 경우 종료
-        const same = all.every(v => v === all[0]);
         if (same) {
-            if (all[0] == 0) answer[0] += 1;
-            else answer[1] += 1;
-            
+            answer[first] += 1;
             return ;
-        }
-        
-        // 원소가 4개인 경우,
-        if (len == 2) {
-            const zeroSum = all.filter(v => v === 0).length;
-            answer[0] += zeroSum;
-            answer[1] += all.length - zeroSum;
-            
-            return;
         }
         
         // 4개로 쪼개기
