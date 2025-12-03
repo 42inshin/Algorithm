8986a8f99ceead1f2ac77c5c85d612e1d10b6526
INHO SHIN <inshin@student.42seoul.kr>
2025-12-03 19:33:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 22.62 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,44 +1,23 @@
 function solution(bridge_length, weight, truck_weights) {
     // 경과 시간
     let time = 0;
-    let trucks = [...truck_weights].reverse();
-    
-    
-    // 대기에서 다리로 옮긴 마지막 트럭 인덱스
-    let bridgeIndex = 0;
-    // 완료한 트럭의 인덱스
-    let completeIndex = 0;
-    // 다리 위의 차들
-    const trucksOnBridge = new Map();
-    let trucksIndexOnBridge = [];
+    // 다리 위: [차 무게, 다리 지나갈 시간]
+    let q = [[0,0]]
     // 다리 위 무게
-    let weightOnBridge = 0;
-
-    while (trucks.length > 0 || trucksIndexOnBridge.length > 0) {
+    let weight_on_bridge = 0;
+    
+    while (truck_weights.length > 0 || q.length > 0) {
+        // 다리 건너기
+        if (q[0][1] == time) weight_on_bridge -= q.shift()[0];
         
-        // 2. 다리에서 지나는 차들
-        for (let i = completeIndex; i < bridgeIndex; i++) {
-            let carTime = trucksOnBridge.get(i)
-            if (carTime < bridge_length) {
-                trucksOnBridge.set(i, carTime + 1);
-            } else {
-                trucksIndexOnBridge.shift();
-                trucksOnBridge.delete(i);
-                weightOnBridge -= truck_weights[i]
-                completeIndex = i + 1;
-            }    
-        }
-        
-        // 1. 다리에 올리기
-        let waitTruck = trucks.at(-1);
-        if (waitTruck && weightOnBridge + waitTruck <= weight) {
-            let oneTruck = trucks.pop();
-            trucksOnBridge.set(bridgeIndex, 1);
-            trucksIndexOnBridge.push(bridgeIndex);
-            ++bridgeIndex;
-            // 다리에 올라간 차 무게
-            weightOnBridge += oneTruck;
-        }
+        // 대기 트럭 다리 건널수 있는지 확인
+        if (weight_on_bridge + truck_weights[0] <= weight) {
+            weight_on_bridge += truck_weights[0];
+            q.push([truck_weights.shift(), time + bridge_length]);
+        } 
+        // else {
+        //     time = q[0][1] - 1;
+        // }
         
         time++;
     }
