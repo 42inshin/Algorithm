43f554235bac3759c24f986d97fea303eae0e704
INHO SHIN <inshin@student.42seoul.kr>
2025-12-11 20:37:13 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -1,24 +1,7 @@
 function solution(storey) {
-    let count = 0;
+    if (storey < 5) return storey;
+    let r = storey % 10;
+    let m = (storey - r) / 10;
     
-    // 1, 10, 100, ... 순으로 절반 기준(5)으로 위로 보낼지 아래로 보낼지 선택하기
-    while (storey > 0) {
-        let remain = storey % 10;
-        
-        if (remain == 5 && storey % 100 >= 50) {
-        // remain 이 5일때는 조건 추가해야 함. 십의 자리가 5이상이면 위로 가야 맞음
-            storey += 10 - remain;
-            count += 10 - remain;
-        } else if (remain > 5) {
-            // 1의 자리 숫자가 5보다 클때 위로 보내기
-            storey += 10 - remain;
-            count += 10 - remain;
-        } else if (remain != 0) {
-            storey -= remain;
-            count += remain;
-        }
-        storey /= 10;
-    }
-    
-    return count;
+    return Math.min(r + solution(m), 10 - r + solution(m + 1));
 }
