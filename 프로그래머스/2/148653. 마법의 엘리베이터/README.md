1270ebcba2064e3f752e87262ce3f59013a7575a
INHO SHIN <inshin@student.42seoul.kr>
2025-12-11 19:55:58 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 11일 19:17:35
+2025년 12월 11일 19:55:55
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -5,12 +5,11 @@ function solution(storey) {
     while (storey > 0) {
         let remain = storey % 10;
         
-        
         if (remain == 5 && storey % 100 >= 50) {
-        // remain 이 5일때는 조건 추가해야 함. 십의 자리가 5이상이면 위로    
+        // remain 이 5일때는 조건 추가해야 함. 십의 자리가 5이상이면 위로 가야 맞음
             storey += 10 - remain;
             count += 10 - remain;
-        } else if (remain > 5) { // > 일때 1,3,12 틀림 / >= 일때 5,6,8,9 틀림
+        } else if (remain > 5) {
             // 1의 자리 숫자가 5보다 클때 위로 보내기
             storey += 10 - remain;
             count += 10 - remain;
