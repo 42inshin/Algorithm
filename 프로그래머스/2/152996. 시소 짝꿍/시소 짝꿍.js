f60f33a1884ba627b21aa91cba14aa689f3faa93
INHO SHIN <inshin@student.42seoul.kr>
2025-12-14 13:57:13 +0900

    [level 2] Title: 시소 짝꿍, Time: 56.70 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,16 +1,17 @@
 function solution(weights) {
-    let answer = 0;
-    const m = {}
+    let count = 0;
+    const map = {};
     
-    weights.sort((a,b) => b - a);
+    weights.sort((a, b) => b - a)
     
     for (let w of weights) {
-        if (m[w]) answer += m[w];
-        if (m[w * 3 / 2]) answer += m[w * 3 / 2]
-        if (m[w * 2]) answer += m[w * 2]
-        if (m[w * 4 / 3]) answer += m[w * 4 / 3]
+        if (map[w]) count += map[w]
+        if (map[w * 2]) count += map[w * 2]
+        if (map[w * 3 / 2]) count += map[w * 3 / 2]
+        if (map[w * 4 / 3]) count += map[w * 4 / 3]
         
-        m[w] = (m[w] || 0) + 1;
+        map[w] = (map[w] || 0) + 1
     }
-    return answer;
+    
+    return count;
 }
