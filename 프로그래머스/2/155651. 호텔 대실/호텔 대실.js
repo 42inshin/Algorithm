837f46c37b76cb88f2da67a74a0d5888d6ed06b4
INHO SHIN <inshin@student.42seoul.kr>
2025-12-18 13:42:20 +0900

    [level 2] Title: 호텔 대실, Time: 25.30 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -12,6 +12,7 @@ function solution(book_time) {
         events.push([toMin(e) + cleaningTime, -1]);
     }
     
+    // 시간 순으로 정렬. 동일한 시간은 종료 시간 먼저 처리
     events.sort((a,b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0])
     
     let max = 0;
