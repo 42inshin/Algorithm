a7bd4cb3c2d7d6d4ad88ce857ef78f14e73667ea
INHO SHIN <inshin@student.42seoul.kr>
2026-01-31 08:37:32 +0900

    [level 2] Title: 타겟 넘버, Time: 39.00 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,18 +1,22 @@
 function solution(numbers, target) {
     var answer = 0;
     
-    dfs(0, 0)
-
+    // +, - 별로 계속 합해야 함. 끝까지 가야하므로 dfs
+    
     function dfs(index, sum) {
-        if (index == numbers.length) {
-            if (sum == target)
+        // 종료조건: 끝까지 봤을때
+        if (index === numbers.length) {
+            if (sum === target) {
                 answer++;
-            return ;
+            }
+            return;
         }
         
         dfs(index + 1, sum + numbers[index]);
         dfs(index + 1, sum - numbers[index]);
-    }    
+    }
     
+    dfs(0, 0)
+ 
     return answer;
 }
