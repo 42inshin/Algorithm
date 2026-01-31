61fd11f4a4b6c56bb6d7d3fa6b294d7d8b3a94fb
INHO SHIN <inshin@student.42seoul.kr>
2026-01-31 22:37:52 +0900

    [level 3] Title: 단어 변환, Time: 0.93 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -15,22 +15,22 @@ func canChange(from: String, to: String, done: [String]) -> Bool {
 }
 
 func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
-    var queue: [([String], Int)] = []
+    var queue: [[String]] = []
     var ans = 0
     
-    queue.append(([begin], 0))
+    queue.append([begin])
     while !queue.isEmpty {
         var pop = queue.removeFirst()
-        guard let last = pop.0.last else { break }
+        guard let last = pop.last else { break }
 
         if last == target { 
-            ans = pop.1
+            ans = pop.count - 1
             break
         }
         for word in words {
-            if canChange(from: last, to: word, done: pop.0) {
-                let newArr = pop.0 + [word]
-                queue.append((newArr, pop.1 + 1))
+            if canChange(from: last, to: word, done: pop) {
+                let newArr = pop + [word]
+                queue.append(newArr)
             }
         }
     }
