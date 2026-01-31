0ae29c28af67813b3b7c72acd40f91df9c036097
INHO SHIN <inshin@student.42seoul.kr>
2026-01-31 09:18:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.28 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -6,16 +6,12 @@ function solution(maps) {
     const dy = [1, -1, 0, 0];
     const dx = [0, 0, 1, -1];
     
-    // 0, 0에서 1칸부터 시작
-    const q = [[0, 0, 1]];
+    // y, x, dist
+    const queue = [[0, 0, 1]]
     
-    // 방문처리: 벽으로 만들어 안오게 처리
-    maps[0][0] = 0;
-    
-    while (q.length > 0) {
-        const [y, x, dist] = q.shift();
+    while (queue.length) {
+        const [y, x, dist] = queue.shift();
         
-        // 탈출 조건: 도착지점
         if (y === n - 1 && x === m - 1) return dist;
         
         for (let i = 0; i < 4; i++) {
@@ -24,7 +20,7 @@ function solution(maps) {
             
             if (ny >= 0 && ny < n && nx >= 0 && nx < m && maps[ny][nx] === 1) {
                 maps[ny][nx] = 0;
-                q.push([ny, nx, dist + 1]);
+                queue.push([ny, nx, dist + 1]);
             }
         }
     }
