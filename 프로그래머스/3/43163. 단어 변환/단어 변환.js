73dd905e822ca9a2125413d6cfad73917bf33231
INHO SHIN <inshin@student.42seoul.kr>
2026-01-31 21:55:12 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,34 @@
+function solution(begin, target, words) {
+    const used = [];
+    const stack = [[begin, 0]];
+    
+    if (!words.includes(target)) return 0;
+    
+    while (stack.length) {
+        const [word, count] = stack.shift();
+        // 동일하면 종료
+        if (word === target) return count;
+        
+        if (used.includes(word)) continue;
+        used.push(word);
+        
+        for (let w of words) {
+            // 1개 차이 나면
+            if (isOneChange(word, w)) {
+                stack.push([w, count + 1]);
+            }
+        }
+    }
+    
+    return 0;
+}
+
+function isOneChange(cur, next) {
+    let count = 0;
+    
+    for (let i = 0; i < cur.length; i++) {
+        if (cur[i] !== next[i]) count++;
+    }
+    
+    return count === 1 ? true : false;
+}
