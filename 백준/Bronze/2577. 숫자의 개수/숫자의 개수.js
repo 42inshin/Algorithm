ecc4f48ba3fdd5002d3c2276ede8dfbc696ce1cb
INHO SHIN <inshin@student.42seoul.kr>
2026-01-31 16:05:11 +0900

    [Bronze II] Title: 숫자의 개수, Time: 84 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -2,7 +2,10 @@ const fs = require("fs");
 const numbers = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
 
 const multi = numbers.reduce((acc, cur) => acc * cur, 1).toString();
+const count = new Array(10).fill(0);
 
-for (let i = 0; i < 10; i++) {
-  console.log(multi.split("").filter(v => v === i.toString()).length)
+for (let char of multi) {
+  count[Number(char)]++;
 }
+
+console.log(count.join("\n"))
