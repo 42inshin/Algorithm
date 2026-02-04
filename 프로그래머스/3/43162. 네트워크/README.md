ca6636828629bd8e3d4ca428e1db6837b17083f4
INHO SHIN <inshin@student.42seoul.kr>
2026-02-05 01:22:50 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.45 ms
+메모리: 33.7 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 31일 08:00:23
+2026년 02월 05일 01:22:45
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,13 +1,38 @@
+// 3일 후
 function solution(n, computers) {
-    var answer = 0;
-    let visited = new Array(n).fill(false);
+    let answer = 0;
+    const visited = new Array(n).fill(false);
     
-    const dfs = (node) => {
+    function dfs(node) {
+        visited[node] = true;
+        for (let i = 0; i < n; i++) {
+            if (computers[node][i] === 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        if (visited[i]) continue;
+        dfs(i);
+        answer++;
+    }
+    return answer;
+}
+
+
+// dfs
+
+function solution1(n, computers) {
+    var answer = 0;
+    const visited = new Array(n).fill(false);
+    
+    function dfs(node) {
         visited[node] = true;
         
-        for (let neighbor = 0; neighbor < n; neighbor++) {
-            if (computers[node][neighbor] === 1 && !visited[neighbor]) {
-                dfs(neighbor);
+        for (let j = 0; j < n; j++) {
+            if (computers[node][j] === 1 && !visited[j]) {
+                dfs(j);
             }
         }
     }
@@ -16,6 +41,33 @@ function solution(n, computers) {
         if (!visited[i]) {
             dfs(i);
             answer++;
+        }
+    }
+    
+    return answer;
+}
+
+// bfs
+function solution2(n, computers) {
+    let answer = 0;
+    const visited = new Array(n).fill(false);
+    
+    for (let i = 0; i < n; i++) {
+        if (visited[i]) continue;
+        
+        answer++;
+        visited[i] = true;
+        const q = [i];
+        
+        while (q.length > 0) {
+            const node = q.shift();
+
+            for (let j = 0; j < n; j++) {
+                if (computers[node][j] === 1 && !visited[j]) {
+                    visited[j] = true;
+                    q.push(j);
+                }
+            }
         }
     }
     
